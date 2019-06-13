06ac6c7700ace19fe41b827b661a956727894348
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-06-13 14:59:50 -0500

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
-#include "delaunay_triangulation.h"
+#include "base_dt.hpp"
 
 using namespace std;
 
 int main (void)
 {
+	Point<2> p;
 	//TODO : Write main program with structures from delaunay_triangulation.h
 	
 	return 0;
