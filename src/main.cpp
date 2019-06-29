28ca4a23125dfa2a91813d86a28af9612935084c
César Salcedo <cesar.salcedo@utec.edu.pe>
2019-06-29 18:17:05 -0500

    Example of point distribution generator in src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
-#include <window/canvas.hpp>
+// #include <window/canvas.hpp>
+#include <iostream>
+
+#include <geometry/point.hpp>
+#include <generator/generator.hpp>
 
 using namespace std;
 
 int main (void)
 {
-	Window w;
-	w.display(); 
+//	Window w;
+//	w.display(); 
+	generator::NormalDistribution<2, float> point_generator;
+
+	std::vector<geometry::Point<2, float>> points;
+	
+	for (int i = 0; i < 10; i++)
+	{
+		points.push_back(point_generator());
+	}
+
+	for (int i = 0; i < points.size(); i++)
+	{
+		cout << points[i] << endl;
+	}
+
+	
 
 	return 0;
 };
