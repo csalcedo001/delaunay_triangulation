803e1e845444c5f1f80744bc6fb33bfec7e6746f
María <mlovatonv@gmail.com>
2019-07-04 13:57:57 -0500

    Add test

--- src/main.cpp
+++ src/main.cpp
@@ -7,44 +7,33 @@
 
 int main (void)
 {
-	// vector<Point<3, float>> points;
-	// array<float, 3> coordinates;
-	// coordinates[0] = 0;
-	// coordinates[1] = 0;
-	// coordinates[2] = 0;
-	// points.emplace_back(coordinates);
-	// coordinates[0] = 1;
-	// coordinates[1] = 1;
-	// coordinates[2] = 1;
-	// points.emplace_back(coordinates);
-	// Window w(points);
-	// w.display();
-
 	generator::NormalDistribution<3, float> point_generator;
 
-	std::vector<geometry::Point<3, float>*> points;
+    std::vector<geometry::Point<3, float>> points;
+	std::vector<geometry::Point<3, float>*> points_ptr;
 	
 	for (int i = 0; i < 10; i++)
 	{
 		geometry::Point<3, float> point = point_generator();
 		geometry::Point<3, float> *point_ptr = new Point<3, float>(point.coordinates_);
-		points.push_back(point_ptr);
+		points.push_back(point);
+		points_ptr.push_back(point_ptr);
 	}
 
 	for (int i = 0; i < points.size(); i++)
 	{
-		std::cout << *points[i] << std::endl;
+		std::cout << points[i] << std::endl;
 	}
 
-	geometry::Triangulation<3, float> t(points);
+	geometry::Triangulation<3, float> t(points_ptr);
 	std::vector<geometry::Triangle<3, float>> triangles = t.incremental_triangulation();
 	std::cout << "Finished";
 
-	// Window<Point<3, float>> w1(2, points);
-	// w1.display();
+    Window<Point<3, float>> w1(2, points);
+	w1.display();
 
-	// Window<Triangle<3, float>> w2(1, ());
-	// w2.display();
+	Window<Triangle<3, float>> w2(1, triangles);
+	w2.display();
 
 	return 0;
 };
